c52a1df6b1dc957a7ded1296f312b287e5b25eb3
Jason Chung <zzxworld@gmail.com>
2018-11-18 11:55:59 +0800

    update

--- resources/views/admin/page/edit.blade.php
+++ resources/views/admin/page/edit.blade.php
@@ -6,6 +6,11 @@
 
     <div class="form-group">
         <a class="btn btn-outline-secondary" href="{{ url('admin/pages') }}">返回</a>
+        <form action="{{ url('admin/pages/'.$page->id) }}" method="post">
+            {{ csrf_field() }}
+            @method('DELETE')
+            <button class="btn btn-danger float-right" type="submit">删除页面</button>
+        </form>
     </div>
 
     <form action="{{ url('admin/pages/'.$page->id) }}" method="post">
@@ -14,3 +19,23 @@
     </form>
 
 @endsection
+
+@section('head')
+    <style type="text/css" media="screen">
+    .form-group form {
+        display: inline;
+    }
+    </style>
+@endsection
+
+@section('foot')
+    <script charset="utf-8" defer="defer">
+        document.querySelector('.btn-danger').addEventListener('click', event => {
+            event.preventDefault()
+            if (confirm('确定要删除此页面吗?')) {
+                event.target.parentNode.submit()
+            }
+        })
+    </script>
+@endsection
+
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,10 +6,12 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ url('css/app.css') }}" type="text/css" charset="utf-8">
     <script charset="utf-8" src="{{ url('js/app.js') }}" defer="defer"></script>
+    @yield('head')
 </head>
 <body>
     <div class="container">
         @yield('content')
     </div>
+    @yield('foot')
 </body>
 </html>
